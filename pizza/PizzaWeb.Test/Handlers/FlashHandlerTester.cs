06861cab72509edab1af668bd9abf0c5ad2af670
Chad Myers <chad.myers@gmail.com>
2012-02-27 21:05:18 -0600

    Add handler tests

---
+++ pizza/PizzaWeb.Test/Handlers/FlashHandlerTester.cs
@@ -0,0 +1,35 @@
+﻿using FubuMVC.Core.SessionState;
+using FubuTestingSupport;
+using NUnit.Framework;
+using PizzaWeb.Handlers.Flashes;
+using Rhino.Mocks;
+
+namespace PizzaWeb.Test.Handlers
+{
+    [TestFixture]
+    public class FlashHandlerTester : InteractionContext<get_handler>
+    {
+        [Test]
+        public void should_be_empty_if_no_flashes()
+        {
+            ClassUnderTest.Execute(null).ShouldBeEmpty();
+        }
+
+        [Test]
+        public void should_render_a_ul_tag()
+        {
+            Services.Get<IFlash>().Stub(f => f.Retrieve<string[]>()).Return(new[] {"foo"});
+            ClassUnderTest.Execute(null).ShouldStartWith("<ul>");
+        }
+
+        [Test]
+        public void should_render_an_li_for_each_flash()
+        {
+            var flashes = new[] {"foo", "baz"};
+            Services.Get<IFlash>().Stub(f => f.Retrieve<string[]>()).Return(flashes);
+            var list = ClassUnderTest.Execute(null);
+            list.ShouldContain(">foo<");
+            list.ShouldContain(">baz<");
+        }
+    }
+}
---
+++ pizza/PizzaWeb.Test/Handlers/PlaceOrderPostHandlerTester.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using FubuMVC.Core.Continuations;
+using FubuMVC.Core.SessionState;
+using FubuTestingSupport;
+using NUnit.Framework;
+using PizzaWeb.Data;
+using PizzaWeb.Handlers.Home;
+using PizzaWeb.Handlers.PlaceOrder;
+using Rhino.Mocks;
+
+namespace PizzaWeb.Test.Handlers
+{
+    [TestFixture]
+    public class PlaceOrderPostHandlerTester : InteractionContext<post_handler>
+    {
+        private PickupOrder order;
+        private FubuContinuation result;
+
+        protected override void beforeEach()
+        {
+            order = new PickupOrder { Id = Guid.NewGuid() };
+            result = ClassUnderTest.Execute(order);
+        }
+
+        [Test]
+        public void should_save_order()
+        {
+            Services.Get<IRepository>().AssertWasCalled(r => r.Save(order));
+        }
+
+        [Test]
+        public void should_flash()
+        {
+            Services.Get<IFlash>().AssertWasCalled(
+                r => r.Flash(Arg<String[]>.Matches(s => s.Length == 1)));
+        }
+
+        [Test]
+        public void should_flash_success()
+        {
+            Services.Get<IFlash>().AssertWasCalled(
+                r => r.Flash(Arg<String[]>.Matches(s => s[0].Contains("success"))));
+        }
+
+        [Test]
+        public void should_flash_orderID()
+        {
+            Services.Get<IFlash>().AssertWasCalled(
+                r => r.Flash(Arg<String[]>.Matches(s => s[0].Contains(order.Id.ToString()))));
+        }
+
+        [Test]
+        public void should_redirect_back_to_home()
+        {
+            result.AssertWasRedirectedTo<HomeModel>(h => h != null);
+        }
+    }
+}
