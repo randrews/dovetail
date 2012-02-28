a6e7272107b7854471ec243f9e25353b0798bb4c
Chad Myers <chad.myers@gmail.com>
2012-02-27 21:05:23 -0600

    Add rakefile

---
+++ rakefile.rb
@@ -0,0 +1,29 @@
+COMPILE_TARGET = ENV['config'].nil? ? "Debug" : ENV['config'] # Keep this in sync w/ VS settings since Mono is case-sensitive
+CLR_TOOLS_VERSION = "v4.0.30319"
+
+buildsupportfiles = Dir["#{File.dirname(__FILE__)}/buildsupport/*.rb"]
+raise "Run `git submodule update --init` to populate your buildsupport folder." unless buildsupportfiles.any?
+buildsupportfiles.each { |ext| load ext }
+
+include FileTest
+require 'albacore'
+
+desc "**Default**, compiles and runs tests"
+task :default => [:compile, :unit_test]
+
+desc "Target used for the CI server"
+task :ci => [:update_all_dependencies, :default]
+
+desc "Compiles the app"
+task :compile => [:restore_if_missing] do
+  MSBuildRunner.compile :compilemode => COMPILE_TARGET, :solutionfile => 'pizza/Pizza.sln', :clrversion => CLR_TOOLS_VERSION
+end
+
+desc "Runs unit tests"
+task :test => [:unit_test]
+
+desc "Runs unit tests"
+task :unit_test => :compile do
+  runner = NUnitRunner.new :compilemode => COMPILE_TARGET, :source => 'pizza', :platform => 'x86'
+  runner.executeTests ['PizzaWeb.Test']
+end
