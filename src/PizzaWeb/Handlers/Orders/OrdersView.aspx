565f752b6600485b2dad2a5cb305ffe4cb2993cf
Ross Andrews <randrews@geekfu.org>
2014-07-24 21:22:36 -0500

    Stub view for listing orders

---
+++ src/PizzaWeb/Handlers/Orders/OrdersView.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" MasterPageFile="../../Content/Main.master" Inherits="PizzaWeb.Handlers.Orders.OrdersView" Title="Hipster Pi: All orders" %>
+<%@ Import Namespace="PizzaWeb.Handlers.Home" %>
+
+<asp:Content ContentPlaceHolderID="MainContent" runat="server">
+        <div class="page-header">
+          <h1>Orders for Pickup</h1>
+        </div>
+        <div class="row">
+          <div class="span3">
+              <p><%= this.LinkTo<HomeModel>().Text("Back home") %></p>
+          </div>
+          <div class="span9">
+              Blah
+          </div>
+        </div>
+        
+</asp:Content>
---
+++ src/PizzaWeb/Handlers/Orders/get_handler.cs
@@ -0,0 +1,27 @@
+﻿using FubuMVC.Core.SessionState;
+using FubuMVC.WebForms;
+using PizzaWeb.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PizzaWeb.Handlers.Orders
+{
+    public class get_handler
+    {
+        private IRepository _repository;
+
+        public get_handler(IRepository repository, IFlash flash)
+        {
+            _repository = repository;
+        }
+
+        public IEnumerable<PickupOrder> Execute()
+        {
+            return _repository.GetAll<PickupOrder>();
+        }
+    }
+
+    public class OrdersView : FubuPage<IEnumerable<PickupOrder>> { }
+}
